d33f8a4a8592e9d68c900a0225ca58aa9f95164e
MohammadHMazarei <mazareimohammadhosein@gmail.com>
2022-10-05 15:09:54 +0330

    GET all tours handler function and POST new tour handler functtion done

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,10 +1,30 @@
+const catchAsync = require('./../utils/catchAsync');
 const Tour = require('./../models/tourModel');
 
-exports.getAllTours = (req, res, next) => {};
+exports.getAllTours = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      data: tours,
+    },
+  });
+});
 
 exports.getTour = (req, res, next) => {};
 
-exports.createTour = (req, res, next) => {};
+exports.createTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
 
 exports.updateTour = (req, res, next) => {};
 
