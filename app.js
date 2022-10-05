08ed42bb756acc942741e0a0abeb15fa1625ba77
MohammadHMazarei <mazareimohammadhosein@gmail.com>
2022-10-05 14:00:40 +0330

    add tour route

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const tourRouter = require('./routes/tourRoute');
 
 const app = express();
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Routes
+app.use('/api/v1/tours', tourRouter);
 
 module.exports = app;
