69eb17b8a62d22d1e390ab7c893996af4d1ed143
MohammadHMazarei <mazareimohammadhosein@gmail.com>
2022-10-05 13:29:01 +0330

    tourController created and added CRUD functions

---
+++ controllers/tourController.js
@@ -0,0 +1,9 @@
+const Tour = require('./../models/tourModel');
+
+exports.getAllTours = (req, res, next) => {};
+
+exports.getTour = (req, res, next) => {};
+
+exports.createTour = (req, res, next) => {};
+
+exports.deleteTour = (req, res, next) => {};
