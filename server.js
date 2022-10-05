f812c20f202f559c474ae01aac986f4d50bd761b
MohammadHMazarei <mazareimohammadhosein@gmail.com>
2022-10-05 11:41:22 +0330

    Create connection to database

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE_LOCAL;
+console.log(DB);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB Connection succesfull!'));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
