b39128f7cfb9df3097850802b085ae605759f1a8
MohammadHMazarei <mazareimohammadhosein@gmail.com>
2022-10-09 10:16:54 +0330

    handle unhandled rejections

--- server.js
+++ server.js
@@ -25,3 +25,11 @@ const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLER REJECTION!!!  Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
