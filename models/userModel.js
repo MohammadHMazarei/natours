fcb4e074aea0fe2c1dad886d905edf5a4f2633c5
MohammadHMazarei <mazareimohammadhosein@gmail.com>
2022-10-09 11:07:02 +0330

    user model completed

---
+++ models/userModel.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please enter your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  role: {
+    type: String,
+    enum: ['user', 'guide', 'lead-guide', 'admin'],
+    default: 'user',
+  },
+  password: {
+    type: String,
+    required: [true, 'A user must have a password'],
+    min: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function (pass) {
+        return pass === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
+  },
+  passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
