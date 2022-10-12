dcd7d6785502ecfc3a39f07d510af77cbb6f3d8d
MohammadHMazarei <mazareimohammadhosein@gmail.com>
2022-10-12 20:46:26 +0330

    createSendToken completed- create user token and send it via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,3 +8,28 @@ const signToken = (id) => {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password from output
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+};
