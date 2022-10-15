c893041a80303ce1f1192a49f62d5763dc1c7d3e
MohammadHMazarei <mazareimohammadhosein@gmail.com>
2022-10-15 11:44:38 +0330

    getMe middleware and getUser handler completed - in middleware we set user id to req.params - in getUser handler function we get user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,24 @@
 const User = require('./../models/userModel');
 const AppError = require('./../utils/appError');
 const catchAsync = require('./../utils/catchAsync');
+
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user;
+
+  next();
+};
+
+exports.getUser = catchAsync(async (req, res, next) => {
+  const user = await User.findOne(req.params.id);
+
+  if (!user) {
+    return next('No user found with that ID', 404);
+  }
+
+  res.status(200).json({
+    status: 'success',
+    detail: {
+      user,
+    },
+  });
+});
