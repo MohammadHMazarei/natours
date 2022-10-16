2ec0d35a27ff3f91c8255536574b3e3c1d531371
MohammadHMazarei <mazareimohammadhosein@gmail.com>
2022-10-16 08:54:49 +0330

    ADD: updateMe handler - This function update current user data but only name and email

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,15 @@ const User = require('./../models/userModel');
 const AppError = require('./../utils/appError');
 const catchAsync = require('./../utils/catchAsync');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+
+  return newObj;
+};
+
 exports.getMe = (req, res, next) => {
   req.params.id = req.user;
 
@@ -19,6 +28,32 @@ exports.getUser = catchAsync(async (req, res, next) => {
     status: 'success',
     detail: {
       user,
+    },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password update. Please use /updateMyPassword',
+        400
+      )
+    );
+  }
+
+  // 2) update user document
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
     },
   });
 });
