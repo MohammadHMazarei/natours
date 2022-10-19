33b71136b44bd648b7709090d5c7cc7d7b9f0deb
MohammadHMazarei <mazareimohammadhosein@gmail.com>
2022-10-19 12:40:04 +0330

    ADD: reviewRouter handler

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoute');
 const userRouter = require('./routes/userRoute');
+const reviewRouter = require('./routes/reviewRoute');
 const globalErrorhandler = require('./controllers/errorController');
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json({ limit: '10kb' }));
 // Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
