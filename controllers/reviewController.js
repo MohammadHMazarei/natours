d3e79e7677e70f92b78c053e261cfeff13ce59b9
MohammadHMazarei <mazareimohammadhosein@gmail.com>
2022-10-19 12:41:13 +0330

    ADD: getAllReviews handler

---
+++ controllers/reviewController.js
@@ -0,0 +1,13 @@
+const Review = require('./../models/reviewModel');
+
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const reviews = await Review.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: reviews.length,
+    data: {
+      data: reviews,
+    },
+  });
+});
