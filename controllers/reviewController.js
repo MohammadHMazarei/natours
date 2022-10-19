9b59dd2a77c1e342df2f85b03af5d888ea2e8e17
MohammadHMazarei <mazareimohammadhosein@gmail.com>
2022-10-19 12:59:46 +0330

    ADD: create review handler

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,4 +1,5 @@
 const Review = require('./../models/reviewModel');
+const catchAsync = require('./../utils/catchAsync');
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
   const reviews = await Review.find();
@@ -8,6 +9,17 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
     results: reviews.length,
     data: {
       data: reviews,
+    },
+  });
+});
+
+exports.createReview = catchAsync(async (req, res, next) => {
+  const review = await Review.create(req.body);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      review,
     },
   });
 });
