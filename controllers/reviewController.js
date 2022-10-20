7b6f2348358a42c009f58fa9e26d01ff82b3c7f3
MohammadHMazarei <mazareimohammadhosein@gmail.com>
2022-10-20 14:30:31 +0330

    ADD: getReview handler

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,3 +1,4 @@
+const AppError = require('../utils/appError');
 const Review = require('./../models/reviewModel');
 const catchAsync = require('./../utils/catchAsync');
 
@@ -15,6 +16,21 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 
 exports.createReview = catchAsync(async (req, res, next) => {
   const review = await Review.create(req.body);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      review,
+    },
+  });
+});
+
+exports.getReview = catchAsync(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(new AppError('No review found with that ID', 404));
+  }
 
   res.status(200).json({
     status: 'success',
