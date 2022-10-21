4b8a10fd67c99ab32304cbdf1a180bdf9e4ae974
MohammadHMazarei <mazareimohammadhosein@gmail.com>
2022-10-21 11:49:07 +0330

    ADD: refactor getReview with getOne handler factory

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -26,21 +26,7 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getReview = catchAsync(async (req, res, next) => {
-  const review = await Review.findById(req.params.id);
-
-  if (!review) {
-    return next(new AppError('No review found with that ID', 404));
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      review,
-    },
-  });
-});
-
+exports.getReview = factory.getOne(Review);
 exports.createReview = factory.createOne(Review);
 exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
