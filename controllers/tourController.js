35810f377ff600b796e01e858b683b9e74ef7abb
MohammadHMazarei <mazareimohammadhosein@gmail.com>
2022-10-21 12:04:52 +0330

    ADD: refactor getAllTours with getAll handler factory

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,24 +4,7 @@ const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const factory = require('./handlerFactory');
 
-exports.getAllTours = catchAsync(async (req, res, next) => {
-  const features = new APIFeatures(Tour.find(req.params), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate();
-
-  const tours = await features.query;
-
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      data: tours,
-    },
-  });
-});
-
+exports.getAllTours = factory.getAll(Tour);
 exports.getTour = factory.getOne(Tour, {
   path: 'reviews',
 });
