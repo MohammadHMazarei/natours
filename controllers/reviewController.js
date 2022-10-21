5b2d0aed09fbe09de93f88de08d1b629336daa2f
MohammadHMazarei <mazareimohammadhosein@gmail.com>
2022-10-21 11:35:46 +0330

    ADD: createReview with handler factory and setTourUserIds middleware for nested routes

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -3,6 +3,14 @@ const Review = require('./../models/reviewModel');
 const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handlerFactory');
 
+exports.setTourUserIds = (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+
+  next();
+};
+
 exports.getAllReviews = catchAsync(async (req, res, next) => {
   let filter = {};
   if (req.params.tourId) filter = { tour: req.params.tourId };
@@ -14,21 +22,6 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
     results: reviews.length,
     data: {
       data: reviews,
-    },
-  });
-});
-
-exports.createReview = catchAsync(async (req, res, next) => {
-  // Allow nested routes
-  if (!req.body.tour) req.body.tour = req.params.tourId;
-  if (!req.body.user) req.body.user = req.user.id;
-
-  const review = await Review.create(req.body);
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      review,
     },
   });
 });
@@ -48,5 +41,6 @@ exports.getReview = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.createReview = factory.createOne(Review);
 exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
