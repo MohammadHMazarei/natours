93dbc698cfeca411b56963d13e886109f4c3c731
MohammadHMazarei <mazareimohammadhosein@gmail.com>
2022-10-22 12:20:17 +0330

    ADD: tour-within GET endpoint

--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -23,6 +23,10 @@ router
   );
 
 router
+  .route('/tour-within/:distance/center/:latlng/unit/:unit')
+  .get(tourController.getToursWithin);
+
+router
   .route('/')
   .get(tourController.getAllTours)
   .post(
