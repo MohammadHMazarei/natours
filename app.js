b79046e2cd8dbfb2571bc471b69c672358c84f61
MohammadHMazarei <mazareimohammadhosein@gmail.com>
2022-10-23 09:02:09 +0330

    ADD: Security layers

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoute');
 const userRouter = require('./routes/userRoute');
@@ -7,8 +13,40 @@ const globalErrorhandler = require('./controllers/errorController');
 
 const app = express();
 
+// Global middlewares
+// Set security HTTP header
+app.use(helmet);
+
+// Limit request from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 100,
+  message: 'Too many request from this IP, Please try again in an hour!',
+});
+app.use('/api', limiter);
+
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization againts NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization againts xss
+app.use(xss());
+
+// Prevent Paramter Pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
 // Routes
 app.use('/api/v1/tours', tourRouter);
